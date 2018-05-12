1fb2d92e689ec0755bdf345cb390877d49a9eabc
Jeffrei Cher <jeffreicher@yahoo.com>
2018-05-11 21:54:27 -0700

    Removed unused import and added alts to icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,14 +4,13 @@ import Email from '../assets/images/mail.png';
 import Phone from '../assets/images/phone.png';
 import Twitter from '../assets/images/twitter.png';
 import Facebook from '../assets/images/facebook.png';
-import Contact from '../assets/images/contact-image.png';
 
 export default () => (
     <div className="foot-container">
         <div className="contact">
-            <p><img src={Email} /> order@mboutique.com</p> 
-            <p><img src={Phone} /> 949-800-3111</p> 
-            <p><img src={Facebook} /> <img src={Twitter} /> Follow Us</p>
+            <p><img src={Email} alt="Email Icon" /> order@mboutique.com</p> 
+            <p><img src={Phone} alt="Phone Icon" /> 949-800-3111</p> 
+            <p><img src={Facebook} alt="Facebook Icon" /> <img src={Twitter} alt="Twitter Icon" /> Follow Us</p>
         </div>
         <div className="copyright">
             <p>Copyright &copy; MBoutique. All Rights Reserved.</p>
